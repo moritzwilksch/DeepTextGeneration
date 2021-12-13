d7099e01ae0c56961e7cb8e6091f167069bbf2aa
Moritz Wilksch <moritzwilksch@gmail.com>
2021-12-13 05:22:21 +0100

    argparse

--- Makefile
+++ Makefile
@@ -3,3 +3,6 @@ install:
 
 collect-tweets:
 	cd src && python3 -i data_collection.py
+
+train-model0:
+	cd src && python3 -i model0_wordbased.py --embedding_dim 64 --gru_dim 64 --dense_dim 64 --batch_size 256 --learning_rate 0.0001
--- src/model0_wordbased.py
+++ src/model0_wordbased.py
@@ -5,6 +5,18 @@ import io
 import boto3
 import os
 from sklearn.model_selection import train_test_split
+import logging
+import argparse
+
+argparser = argparse.ArgumentParser(description="Train a word-based model")
+argparser.add_argument("--embedding_dim", type=int, default=32)
+argparser.add_argument("--gru_dim", type=int, default=32)
+argparser.add_argument("--dense_dim", type=int, default=32)
+argparser.add_argument("--batch_size", type=int, default=256)
+argparser.add_argument("--learning_rate", type=float, default=0.0001)
+args, _ = argparser.parse_known_args()
+logging.basicConfig(level=logging.INFO)
+
 
 bucket = boto3.resource(
     "s3",
@@ -32,7 +44,7 @@ with io.BytesIO() as f:
 tweets = [line.decode("utf-8").strip() for line in corpus]
 tweets_train, tweets_val = train_test_split(tweets, test_size=0.2, random_state=42)
 
-
+logging.info("Loaded data.")
 #%%
 tokenizer.fit_on_texts(tweets_train)
 raw_seq_train = tokenizer.texts_to_sequences(tweets_train)
@@ -66,8 +78,11 @@ val_seq_x = tf.keras.preprocessing.sequence.pad_sequences(
 train = tf.data.Dataset.from_tensor_slices((train_seq_x, train_seq_y))
 val = tf.data.Dataset.from_tensor_slices((val_seq_x, val_seq_y))
 
+logging.info("Created train sequences")
 #%%
-def get_model(embedding_dim=32, gru_dim=32) -> tf.keras.Model:
+def get_model(
+    embedding_dim=32, gru_dim=32, dense_dim=32, learning_rate=0.0001
+) -> tf.keras.Model:
     model = tf.keras.Sequential()
     model.add(
         tf.keras.layers.Embedding(
@@ -79,16 +94,23 @@ def get_model(embedding_dim=32, gru_dim=32) -> tf.keras.Model:
     )
     # model.add(tf.keras.layers.GRU(units=gru_dim, return_sequences=True))
     model.add(tf.keras.layers.GRU(units=gru_dim))
+    model.add(tf.keras.layers.Dense(dense_dim, activation="relu"))
     model.add(tf.keras.layers.Dense(vocab_size, activation="softmax"))
     model.compile(
         loss="sparse_categorical_crossentropy",
-        optimizer=tf.keras.optimizers.Adam(0.0001),
+        optimizer=tf.keras.optimizers.Adam(learning_rate),
         metrics=["accuracy"],
     )
     return model
 
-model = get_model()
+
+model = get_model(
+    embedding_dim=args.embedding_dim,
+    gru_dim=args.gru_dim,
+    dense_dim=args.dense_dim,
+    learning_rate=args.learning_rate,
+)
 model.summary()
 
 #%%
-model.fit(train.batch(256), epochs=10, validation_data=val)
+model.fit(train.batch(args.batch_size), epochs=10, validation_data=val.batch(512))
